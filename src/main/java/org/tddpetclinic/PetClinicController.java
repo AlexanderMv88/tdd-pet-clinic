a154ed85adfc8378f0ffe7191de99ce2593a0735
AlexanderMv88 <postalforever1988@gmail.com>
2021-02-21 22:17:46 +0300

    Внес изменения в код. Тесты прошли успешно.
    (cherry picked from commit 5ea68ab0697e51d794246ed1ffb8612759861b28)

--- src/main/java/org/tddpetclinic/PetClinicController.java
+++ src/main/java/org/tddpetclinic/PetClinicController.java
@@ -1,4 +1,13 @@
 package org.tddpetclinic;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class PetClinicController {
+
+    @PostMapping("/register")
+    public void regiter(){
+
+    }
 }
