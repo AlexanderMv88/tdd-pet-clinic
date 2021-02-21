4e98d76a2dd22fdacf07db4ebc8458f918ed41b1
AlexanderMv88 <postalforever1988@gmail.com>
2021-02-21 21:27:24 +0300

    Инициализация

---
+++ src/main/java/org/tddpetclinic/TddPetClinicApplication.java
@@ -0,0 +1,13 @@
+package org.tddpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TddPetClinicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/tddpetclinic/TddPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tddpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
