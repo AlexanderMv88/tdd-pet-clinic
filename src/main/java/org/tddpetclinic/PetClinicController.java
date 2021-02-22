392c645e327c51e819109f4d7865def0447b30d7
AlexanderMv88 <postalforever1988@gmail.com>
2021-02-22 14:10:16 +0300

    Внес изменения в код. Тесты регистрации прошли успешно.

--- src/main/java/org/tddpetclinic/PetClinicController.java
+++ src/main/java/org/tddpetclinic/PetClinicController.java
@@ -1,13 +1,16 @@
 package org.tddpetclinic;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PetClinicController {
 
     @PostMapping("/register")
-    public void regiter(){
-
+    public ResponseEntity<AnimalDto> regiter(@RequestBody AnimalDto animalDto){
+        return new ResponseEntity<>(animalDto, HttpStatus.CREATED);
     }
 }
