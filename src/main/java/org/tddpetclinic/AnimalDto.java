343f887f148e4b7fc9bee76f1a25bef654902e0e
AlexanderMv88 <postalforever1988@gmail.com>
2021-02-22 18:19:37 +0300

    Внес изменения в код. Валидация возраста с сообщением проходит успешно

--- src/main/java/org/tddpetclinic/AnimalDto.java
+++ src/main/java/org/tddpetclinic/AnimalDto.java
@@ -3,11 +3,13 @@ package org.tddpetclinic;
 import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Data
 public class AnimalDto {
     @NotBlank(message = "Введите кличку")
     protected String name;
+    @NotNull(message = "Введите возраст")
     protected Integer age;
 
 }
