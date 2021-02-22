dac5e35de856db193d32616ff0468c5feb199eb8
AlexanderMv88 <postalforever1988@gmail.com>
2021-02-22 14:38:30 +0300

    Внес изменения в код. Валидация имени прошла успешно.

--- src/main/java/org/tddpetclinic/AnimalDto.java
+++ src/main/java/org/tddpetclinic/AnimalDto.java
@@ -2,9 +2,11 @@ package org.tddpetclinic;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+
 @Data
 public class AnimalDto {
-
+    @NotBlank
     protected String name;
     protected Integer age;
 
--- src/main/java/org/tddpetclinic/PetClinicController.java
+++ src/main/java/org/tddpetclinic/PetClinicController.java
@@ -6,11 +6,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+
 @RestController
 public class PetClinicController {
 
     @PostMapping("/register")
-    public ResponseEntity<AnimalDto> regiter(@RequestBody AnimalDto animalDto){
+    public ResponseEntity<AnimalDto> regiter(@RequestBody @Valid AnimalDto animalDto){
         AnimalResponseDto animalResponseDto = new AnimalResponseDto();
         animalResponseDto.setId(1L);
         animalResponseDto.setName(animalDto.getName());
