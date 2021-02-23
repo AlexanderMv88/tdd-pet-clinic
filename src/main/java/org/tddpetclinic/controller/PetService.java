fc80aaa9fd7b9e1da1ae1c9300c64399f8b5f917
AlexanderMv88 <postalforever1988@gmail.com>
2021-02-24 00:08:12 +0300

    закрыл тест сохранения для сервисного слоя

--- src/main/java/org/tddpetclinic/controller/PetService.java
+++ src/main/java/org/tddpetclinic/controller/PetService.java
@@ -1,12 +1,31 @@
 package org.tddpetclinic.controller;
 
 
+import org.springframework.stereotype.Service;
+import org.tddpetclinic.PetRepository;
 import org.tddpetclinic.dto.PetDto;
 import org.tddpetclinic.dto.PetResponseDto;
+import org.tddpetclinic.entity.Pet;
 
+@Service
 public class PetService {
 
+    private final PetRepository petRepository;
+
+    public PetService(PetRepository petRepository) {
+        this.petRepository = petRepository;
+    }
+
     public PetResponseDto save(PetDto petDto) {
-        return null;
+        Pet pet = new Pet();
+        pet.setName(petDto.getName());
+        pet.setAge(petDto.getAge());
+        Pet savedPet = petRepository.save(pet);
+
+        PetResponseDto petResponseDto = new PetResponseDto();
+        petResponseDto.setId(savedPet.getId());
+        petResponseDto.setName(savedPet.getName());
+        petResponseDto.setAge(savedPet.getAge());
+        return petResponseDto;
     }
 }
